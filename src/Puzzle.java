f5b1ba6c3388020d4c4708aed9c71b56f515a7a1
justinencarnacion <justin.r.encarnacion@gmail.com>
2024-05-01 09:29:29 -0700

    Completed Words class, Created Puzzle class
    - completed and (possibly finalized) Words class, please add methods if needed
    - Created Puzzle class

---
+++ src/Puzzle.java
@@ -0,0 +1,19 @@
+public class Puzzle extends Words {
+
+    private final char[][] GRID = new char[10][10];
+
+    private boolean canTypeInCell;
+
+
+
+
+    /**
+     * @param wordName
+     * @param wordDescription
+     */
+    public Puzzle(String wordName, String wordDescription) {
+        super(wordName, wordDescription);
+    }
+
+
+}
--- src/Words.java
+++ src/Words.java
@@ -1,27 +1,117 @@
-import java.awt.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-import javax.swing.*;
 
-public class Words extends JFrame {
+public class Words  {
 
-    JPanel panel;
-    JLabel label;
+    private String wordName;
 
-    public Words() {
-        setSize(400, 200);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout( new FlowLayout() );
-        label = new JLabel("Hello Swing!");
-        add(label);
+    private String wordDescription;
+
+    /**
+     * if a word is connected, then there is one letter
+     * in common, if there is one letter in common
+     *
+     */
+    private boolean isConnected;
+
+
+
+
+    private ArrayList<Words> wordBank = new ArrayList<Words>();
+
+    /**
+     *
+     * @param wordName
+     * @param wordDescription
+     */
+    public Words(String wordName, String wordDescription) {
+        this.wordName = wordName;
+        this.wordDescription = wordDescription;
     }
 
-    public static void main(String[] args) {
-        // this will be our driver class
+    /**
+     *
+     * @return
+     */
+    public String getWordName() {return wordName;}
 
-        JFrame frame = new JFrame("Test Frame");
-        frame.setVisible(true);
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name) {this.wordName = name;}
 
+    /**
+     *
+     * @return
+     */
+    public String getWordDescription() {return wordDescription;}
 
+    /**
+     *
+     * @param description
+     */
+    public void setDescription(String description) {this.wordDescription = description;}
+
+    /**
+     *
+     * @param words
+     * @return
+     */
+    public void addWordToWordBank (Words words) {
+        wordBank.add(words);
     }
+    public void addMultipleWords(ArrayList<Words> newWords) {
+        for (Words newWord : newWords) {
+            addWordToWordBank(newWord);
+        }
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<Words> getWordBank() {return wordBank;}
+
+    /**
+     * Checks to see if there are at least 2 connections
+     * between word1 and word2
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public boolean isConnected(Words word1, Words word2) {
+        String w1 = word1.getWordName();
+        String w2 = word2.getWordName();
+        for (int i = 0; i < w1.length() ; i++) {
+            for (int j = 0; j < w2.length(); j++) {
+                return (w2.substring(j, j+1).equals(w1.substring(i, i+1)));
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks to see if the user inputted word equals the set word
+     * in the Crossword cells, if this returns false, the cells will be highlighted
+     * red
+     * This method will be used in later class
+     * @param userWord
+     * @param word1
+     * @return
+     */
+    public boolean isWordInBank(Words userWord, Words word1) {
+        return (userWord.getWordName().toLowerCase().equals(word1.getWordName().toLowerCase()));
+    }
+
+
+
+
+
+
+
 
 }
+
