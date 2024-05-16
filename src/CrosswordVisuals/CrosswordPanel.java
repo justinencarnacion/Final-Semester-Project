7cd9c9632224672039b4a76f7ad203e1de7f0073
Phillip D. Le <32821759+philliple0610@users.noreply.github.com>
2024-05-15 19:43:20 -0700

    Updated CrosswordPanel.java
    phillip change

--- src/CrosswordVisuals/CrosswordPanel.java
+++ src/CrosswordVisuals/CrosswordPanel.java
@@ -11,7 +11,11 @@ import javax.swing.JTextField;
 public class CrosswordPanel extends JPanel {
     private JTextField[][] textFields;
     private char[][] data;
+    private ArrayList<Word> keyWords;
 
+    public void setKey(ArrayList<Word> words) {
+    	keyWords = words;
+    }
 
     public void setCrossword(char[][] array) {
         data = array;
@@ -31,12 +35,9 @@ public class CrosswordPanel extends JPanel {
 
                     if (x == 0 && y == 2) {
                         textFields[x][y] = new LetteredTextField("1");
-                    }
-                    else {
+                    } else {
                         textFields[x][y] = new LetteredTextField("");
-
                     }
-
                     textFields[x][y].setPreferredSize(new Dimension(50, 50));
                     textFields[x][y].setFont(textFields[x][y].getFont().deriveFont(50.0f));
                     textFields[x][y].setDocument(new JTextFieldLimit(1));
@@ -53,7 +54,7 @@ public class CrosswordPanel extends JPanel {
     }
 
     public char[][] getCrossword() {
-        int w = textFields.length;
+		int w = textFields.length;
         int h = textFields[0].length;
         char[][] crossword = new char[w][h];
         for (int x = 0; x < w; x++)
@@ -78,40 +79,20 @@ public class CrosswordPanel extends JPanel {
         return crossword;
     }
 
-    public String toString() {
-        String s = "";
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[0].length; j++) {
-                s += data[i][j];
-            }
-        }
-        return s;
+    public void checkWords() { //NOT DONE
+    	if (textFields != null) {
+    		ArrayList<Word> textWords = wordsFound(getCrossword());
+        	
+        	String wordsCorrect = compare(textWords, keyWords);
+        	printArray(textWords);
+        	System.out.println(wordsCorrect);
+    	}
     }
-
-
-
-    void setGreen() {  //WORK ON THIS PLEASE
-
+    
+    public static void setGreen(Word word) {  //WORK ON THIS PLEASE
+    	
     }
-
-    /*public String wordsFound() { //UNFINISHED
-        char[][] crossword = getCrossword();
-        // Puts all words found horizontal and vertical
-        ArrayList<String> wordsHFound = wordsFoundHorizontal(crossword);
-        ArrayList<String> wordsVFound = wordsFoundVertical(crossword);
-
-
-        String foundWords = "";
-        for (String wordF : list) {
-            for (String wordB : wordBank) {
-                foundWords += wordF + ", ";
-                setGreen(wordF);
-            }
-        }
-        foundWords = foundWords.substring(0,foundWords.length()-2) + " is/are correct";
-    }*/
-
-
+    
     public ArrayList<String> wordsFoundHorizontal(char[][] crossword){ //FINISHED
         ArrayList<String> wordList = new ArrayList<String>();
         for (int i = 0; i < crossword.length; i++) {
@@ -155,15 +136,69 @@ public class CrosswordPanel extends JPanel {
         }
         return wordList;
     }
+    
+    public static ArrayList<Word> wordsFound(char[][] crossword){ //FINISHED
+        ArrayList<Word> wordList = new ArrayList<Word>();
+        for (int i = 0; i < crossword.length; i++) { //finds words horizontally
+            int j = 0;
+            while (j < crossword[0].length) {
+                if ( j != crossword[0].length && crossword[i][j] != '0'){
+                    int k = j;
+                    String word = "";
+                    while (k != crossword[0].length && crossword[i][k] != '0') {
+                        word += crossword[i][k];
+                        k++;
+                    }
+                    wordList.add(new Word(word,i,j,false));
+                    j = k;
+                } else {
+                    j++;
+                }
+            }
+        }
+        for (int i = 0; i < crossword[0].length; i++) { //finds words vertically
+            int j = 0;
+            while (j < crossword.length) {
+                if (j != crossword.length && crossword[j][i] != '0'){
+                    int k = j;
+                    String word = "";
+                    while (k != crossword.length && crossword[k][i] != '0') {
+                        word += crossword[k][i];
+                        k++;
+                    }
+                    wordList.add(new Word(word,i,j,true));
+                    j = k;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return wordList;
+    }
+    
+    public static String compare(ArrayList<Word> wordList, ArrayList<Word> answerList) { //NOT WORKING
+    	String wordsCorrect = "";
+    	for (Word tempWord : wordList) {
+    		for (Word tempKey : answerList) {
+    			if (tempWord.getWordName().equalsIgnoreCase(tempKey.getWordName()) && tempWord.getIsVert() == tempKey.getIsVert() && tempWord.getXPos() == tempKey.getXPos() && tempWord.getYPos() == tempKey.getYPos()) {
+	    			wordsCorrect += tempWord.getWordName() + ", ";
+	    			setGreen(tempWord);
+	    		}
+    		}
+    	}
+    	if (wordsCorrect.length() > 0) {
+    		wordsCorrect = wordsCorrect.substring(0, wordsCorrect.length() - 2) + " is/are correct";
+    	} else {
+    		wordsCorrect = "No words are correct";
+    	}
+    	
+    	return wordsCorrect;
+    }
 
-    public void printArray(ArrayList<String> cw) { //method that prints all words found in array I was using for debugging
+    public void printArray(ArrayList<Word> cw) { //method that prints all words found in array I was using for debugging
         for (int i = 0; i < cw.size(); i++) {
-            System.out.println(cw.get(i));
+            System.out.println(cw.get(i).getWordName());
         }
     }
 
-
-    public char[][] getData() {
-        return data;
-    }
 }
