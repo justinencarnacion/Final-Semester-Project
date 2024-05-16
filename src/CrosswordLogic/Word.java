a25bbc0393d25bf2ac1ed1a524f5dd0b55509844
Phillip D. Le <32821759+philliple0610@users.noreply.github.com>
2024-05-15 20:56:13 -0700

    Update Word.java

--- src/CrosswordLogic/Word.java
+++ src/CrosswordLogic/Word.java
@@ -1,5 +1,3 @@
-package CrosswordLogic;
-
 import java.util.ArrayList;
 
 public class Word {
@@ -7,81 +5,141 @@ public class Word {
     private String wordName;
 
     private String wordDescription;
-
-    private boolean isConnected;
+    
+    private int xPos;
+    
+	private int yPos;
+	
+	private boolean isVertical;
 
     private ArrayList<String> wordList = new ArrayList<String>();
     private ArrayList<String> descriptionList = new ArrayList<String>();
-
     private ArrayList<String> fullList = new ArrayList<String>();
-
+    
     private ArrayList<Word> wordBank = new ArrayList<Word>();
 
 
-    public Word() {
-    }
+    public Word(String w, int xP, int yP, boolean isVert) {
+		wordName = w;
+		xPos = xP;
+		yPos = yP;
+		isVertical = isVert;
+	}
 
-    public void setName(String name) {
-        this.wordName = name;
-    }
+	public Word() {
+		// TODO Auto-generated constructor stub
+	}
 
-    public void setDescription(String description) {
-        this.wordDescription = description;
-    }
-
+	public int getXPos() {
+		return xPos;
+	}
+	
+	public int getYPos() {
+		return yPos;
+	}
+	
+	public boolean getIsVert() {
+		return isVertical;
+	}
+    
     /**
+     *
      * @return
      */
     public String getWordName() {
-        return wordName;
+    	return wordName;
     }
 
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name) {
+    	this.wordName = name;
+    }
 
+    /**
+     *
+     * @return
+     */
     public String getWordDescription() {
-        return wordDescription;
+    	return wordDescription;
     }
 
-    public void addToWordBank() {
-        wordBank.add(this);
+    /**
+     *
+     * @param description
+     */
+    public void setDescription(String description) {
+    	this.wordDescription = description;
     }
 
-    public void addToWordList(String word) {
+    /**
+     *
+     */
+    public void addToWordBank (Word word) {
+        wordBank.add(word);
+    }
+    
+    public void addToWordList (String word) {
         wordList.add(word);
     }
-
-    public void addToDescriptionList(String description) {
+    
+    public void addToDescriptionList (String description) {
         descriptionList.add(description);
     }
-
+    
     public void addToFullList(String word, String description) {
         String s = "";
         s+= word + ":" + description;
         fullList.add(s);
     }
 
-    public String getWordBanktoString() {
+    /**
+     *
+     * @return
+     */
+    public String getWordBankToString() {
         String a = "[";
-        for (Word w : wordBank) {
-            a += w.getWordName() + ":" + w.getWordDescription() + ", ";
+        for (Word w : wordBank ) {
+            a+= w.getWordName() + ":" + w.getWordDescription() + ", ";
         }
-        return (a.substring(0, a.length() - 2) + "]");
+        return (a.substring(0,a.length()-2) + "]");
 
     }
-
     public ArrayList<String> getWordList() {
         return wordList;
     }
-
     public ArrayList<String> getDescriptionList() {
         return descriptionList;
     }
     public ArrayList<String> getFullList() {
         return fullList;
     }
+    public ArrayList<Word> getWordBankList(){
+    	return wordBank;
+    }
 
+    public int compareTo(Word actualWord) {
+        return this.getWordName().compareTo(actualWord.getWordName());
+    }
 
-    public Word getWord() {
-        return this;
+    /**
+     * Pre-Condition: actual word is a word with length > 0
+     * @param actualWord
+     * @return
+     */
+    public int compareLettersTo(String actualWord) {
+        if (this.getWordName().length() == 0) return -99;
+        for (int i = 0; i < this.getWordName().length(); i++) {
+            char userLetter = this.getWordName().toLowerCase().charAt(i);
+            char actualLetter = actualWord.toLowerCase().charAt(i);
+
+            if (userLetter != actualLetter ) return -1;
+
+        }
+        return 1;
+
     }
 
 }
